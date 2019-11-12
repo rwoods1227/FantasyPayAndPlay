d42b98181572926fd3bff41d43a50f4f0e4417f1
Ryan Woods <ryanwoods14@gmail.com>
2019-11-12 14:19:57 -0500

    player model and type

---
+++ FantasyPayAndPlay/server/models/Player.js
@@ -0,0 +1,465 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const PlayerSchema = new Schema({
+  user: 
+    {
+      type: Schema.Types.ObjectId,
+      ref: "user"
+    },
+  name: {
+    type: String,
+    required: false,
+  },
+  team: {
+    type: String,
+    required: false,
+  },
+  position: {
+    type: String,
+    required: false,
+  },
+  IsGameOver: {
+    type: Boolean,
+    default: false
+  },
+  projWPassingAttempts: {
+    type: Number,
+    required: false,
+  },
+  projWPassingCompletions: {
+    type: Number,
+    required: false,
+  },
+  projWPassingYards: {
+    type: Number,
+    required: false,
+  },
+  projWPassingTouchdowns: {
+    type: Number,
+    required: false,
+  },
+  projWPassingInterceptions: {
+    type: Number,
+    required: false,
+  },
+  projWpRushingAttempts: {
+    type: Number,
+    required: false,
+  },
+  projWRushingYards: {
+    type: Number,
+    required: false,
+  },
+  projWRushingTouchdowns: {
+    type: Number,
+    required: false,
+  },
+  projWFumblesLost: {
+    type: Number,
+    required: false,
+  },
+  projWReceivingTargets: {
+    type: Number,
+    required: false,
+  },
+  projWReceptions: {
+    type: Number,
+    required: false,
+  },
+  projWReceivingYards: {
+    type: Number,
+    required: false,
+  },
+  projWReceivingTouchdowns: {
+    type: Number,
+    required: false,
+  },
+  projWTwoPointConversionPasses: {
+    type: Number,
+    required: false,
+  },
+  projWTwoPointConversionRuns: {
+    type: Number,
+    required: false,
+  },
+  projWTwoPointConversionReceptions: {
+    type: Number,
+    required: false,
+  },
+  projWFantasyPoints: {
+    type: Number,
+    required: false,
+  },
+  projWFantasyPointsPPR: {
+    type: Number,
+    required: false,
+  },
+  projWActive: {
+    type: Boolean,
+    required: false,
+  },
+  weeklyPassingAttempts: {
+    type: Number,
+    required: false,
+  },
+  weeklyPassingCompletions: {
+    type: Number,
+    required: false,
+  },
+  weeklyPassingYards: {
+    type: Number,
+    required: false,
+  },
+  weeklyPassingTouchdowns: {
+    type: Number,
+    required: false,
+  },
+  weeklyPassingInterceptions: {
+    type: Number,
+    required: false,
+  },
+  weeklypRushingAttempts: {
+    type: Number,
+    required: false,
+  },
+  weeklyRushingYards: {
+    type: Number,
+    required: false,
+  },
+  weeklyRushingTouchdowns: {
+    type: Number,
+    required: false,
+  },
+  weeklyFumblesLost: {
+    type: Number,
+    required: false,
+  },
+  weeklyReceivingTargets: {
+    type: Number,
+    required: false,
+  },
+  weeklyReceptions: {
+    type: Number,
+    required: false,
+  },
+  weeklyReceivingYards: {
+    type: Number,
+    required: false,
+  },
+  weeklyReceivingTouchdowns: {
+    type: Number,
+    required: false,
+  },
+  weeklyTwoPointConversionPasses: {
+    type: Number,
+    required: false,
+  },
+  weeklyTwoPointConversionRuns: {
+    type: Number,
+    required: false,
+  },
+  weeklyTwoPointConversionReceptions: {
+    type: Number,
+    required: false,
+  },
+  weeklyFantasyPoints: {
+    type: Number,
+    required: false,
+  },
+  weeklyFantasyPointsPPR: {
+    type: Number,
+    required: false,
+  },
+  weeklyActive: {
+    type: Boolean,
+    required: false,
+  },
+  seasonPassingAttempts: {
+    type: Number,
+    required: false,
+  },
+  seasonPassingCompletions: {
+    type: Number,
+    required: false,
+  },
+  seasonPassingYards: {
+    type: Number,
+    required: false,
+  },
+  seasonPassingTouchdowns: {
+    type: Number,
+    required: false,
+  },
+  seasonPassingInterceptions: {
+    type: Number,
+    required: false,
+  },
+  seasonpRushingAttempts: {
+    type: Number,
+    required: false,
+  },
+  seasonRushingYards: {
+    type: Number,
+    required: false,
+  },
+  seasonRushingTouchdowns: {
+    type: Number,
+    required: false,
+  },
+  seasonFumblesLost: {
+    type: Number,
+    required: false,
+  },
+  seasonReceivingTargets: {
+    type: Number,
+    required: false,
+  },
+  seasonReceptions: {
+    type: Number,
+    required: false,
+  },
+  seasonReceivingYards: {
+    type: Number,
+    required: false,
+  },
+  seasonReceivingTouchdowns: {
+    type: Number,
+    required: false,
+  },
+  seasonTwoPointConversionPasses: {
+    type: Number,
+    required: false,
+  },
+  seasonTwoPointConversionRuns: {
+    type: Number,
+    required: false,
+  },
+  seasonTwoPointConversionReceptions: {
+    type: Number,
+    required: false,
+  },
+  seasonFantasyPoints: {
+    type: Number,
+    required: false,
+  },
+  seasonFantasyPointsPPR: {
+    type: Number,
+    required: false,
+  },
+  projSPassingAttempts: {
+    type: Number,
+    required: false,
+  },
+  projSPassingCompletions: {
+    type: Number,
+    required: false,
+  },
+  projSPassingYards: {
+    type: Number,
+    required: false,
+  },
+  projSPassingTouchdowns: {
+    type: Number,
+    required: false,
+  },
+  projSPassingInterceptions: {
+    type: Number,
+    required: false,
+  },
+  projSpRushingAttempts: {
+    type: Number,
+    required: false,
+  },
+  projSRushingYards: {
+    type: Number,
+    required: false,
+  },
+  projSRushingTouchdowns: {
+    type: Number,
+    required: false,
+  },
+  projSFumblesLost: {
+    type: Number,
+    required: false,
+  },
+  projSReceivingTargets: {
+    type: Number,
+    required: false,
+  },
+  projSReceptions: {
+    type: Number,
+    required: false,
+  },
+  projSReceivingYards: {
+    type: Number,
+    required: false,
+  },
+  projSReceivingTouchdowns: {
+    type: Number,
+    required: false,
+  },
+  projSTwoPointConversionPasses: {
+    type: Number,
+    required: false,
+  },
+  projSTwoPointConversionRuns: {
+    type: Number,
+    required: false,
+  },
+  projSTwoPointConversionReceptions: {
+    type: Number,
+    required: false,
+  },
+  projSFantasyPoints: {
+    type: Number,
+    required: false,
+  },
+  projSFantasyPointsPPR: {
+    type: Number,
+    required: false,
+  },
+  projSAverageDraftPositionPPR: {
+    type: Number,
+    required: false,
+  },
+  projSAverageDraftPosition: {
+    type: Number,
+    required: false,
+  }
+});
+
+
+module.exports = mongoose.model("player", PlayerSchema);
+
+
+//weekly projections will have prefix of projW
+//season projections will have prefix of projS
+//weekly stats will have prefix of weekly
+//season stats will have prefix of season
+
+//fantasy projected weekly stats https:/ / api.sportsdata.io / v3 / nfl / projections / json / PlayerGameProjectionStatsByWeek / 2019 / 10
+
+  //FantasyPointsYahoo
+  //Name
+  //Position
+  //Played(0/1)
+  //Started(0/1)
+  //Activated(0/1)
+
+
+  //To begin with stats will not be included but here are the keys
+  //PassingAttempts
+  //PassingCompletions
+  //PassingYards
+  //PassingTouchdowns
+  //PassingInterceptions
+  //RushingAttempts
+  //RushingYards
+  //RushingTouchdowns
+  //FumblesLost
+  //ReceivingTargets
+  //Receptions
+  //ReceivingYards
+  //ReceivingTouchdowns
+  //TwoPointConversionPasses
+  //TwoPointConversionRuns
+  //TwoPointConversionReceptions
+
+  //FantasyPoints
+  //FantasyPointsPPR
+
+  //IsGameOver(true/false)
+
+//Keys from API call for weekly stats https://api.sportsdata.io/v3/nfl/stats/json/PlayerGameStatsByWeek/2019/10
+  //FantasyPointsYahoo
+  //Name
+  //Position
+  //Played(0/1)
+  //Started(0/1)
+  //Activated(0/1)
+
+
+  //To begin with stats will not be included but here are the keys
+  //PassingAttempts
+  //PassingCompletions
+  //PassingYards
+  //PassingTouchdowns
+  //PassingInterceptions
+  //RushingAttempts
+  //RushingYards
+  //RushingTouchdowns
+  //FumblesLost
+  //ReceivingTargets
+  //Receptions
+  //ReceivingYards
+  //ReceivingTouchdowns
+  //TwoPointConversionPasses
+  //TwoPointConversionRuns
+  //TwoPointConversionReceptions
+
+  //FantasyPoints
+  //FantasyPointsPPR
+
+  //IsGameOver(true/false)
+
+//Keys from season stats https://api.sportsdata.io/v3/nfl/stats/json/PlayerSeasonStats/2019 (same free key)
+  //FantasyPointsYahoo
+  //Name
+  //Position
+
+
+  //PassingAttempts
+  //PassingCompletions
+  //PassingYards
+  //PassingTouchdowns
+  //PassingInterceptions
+  //RushingAttempts
+  //RushingYards
+  //RushingTouchdowns
+  //FumblesLost
+  //ReceivingTargets
+  //Receptions
+  //ReceivingYards
+  //ReceivingTouchdowns
+  //TwoPointConversionPasses
+  //TwoPointConversionRuns
+  //TwoPointConversionReceptions
+
+  //FantasyPoints
+  //FantasyPointsPPR
+
+
+
+// projected season stats (for draft stuff)   
+
+  //FantasyPointsYahoo
+  //Name
+  //Position
+  //Played(0/1)
+  //Started(0/1)
+  //Activated(0/1)
+
+
+  //To begin with stats will not be included but here are the keys
+  //PassingAttempts
+  //PassingCompletions
+  //PassingYards
+  //PassingTouchdowns
+  //PassingInterceptions
+  //RushingAttempts
+  //RushingYards
+  //RushingTouchdowns
+  //FumblesLost
+  //ReceivingTargets
+  //Receptions
+  //ReceivingYards
+  //ReceivingTouchdowns
+  //TwoPointConversionPasses
+  //TwoPointConversionRuns
+  //TwoPointConversionReceptions
+
+  //FantasyPoints
+  //FantasyPointsPPR
+  //AverageDraftPositionPPR
+  //AverageDraftPosition
---
+++ FantasyPayAndPlay/server/schema/types/player_type.js
@@ -0,0 +1,103 @@
+const Player = mongoose.model("player");
+const UserType = require("./user_type");
+const User = mongoose.model("user");
+
+const PlayerType = new GraphQLObjectType({
+  name: "PlayerType",
+  // remember we wrap the fields in a thunk to avoid circular dependency issues
+  fields: () => ({
+    _id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    team: { type: GraphQLString },
+    position: { type: GraphQLString },
+    IsGameOver: { type: GraphQLBoolean },
+    line: { type: GraphQLInt },
+    
+    projWPassingAttempts: { type: GraphQLFloat },
+    projWPassingCompletions: { type: GraphQLFloat },
+    projWPassingYards: { type: GraphQLFloat },
+    projWPassingTouchdowns: { type: GraphQLFloat },
+    projWPassingInterceptions: { type: GraphQLFloat },
+    projWpRushingAttempts: { type: GraphQLFloat },
+    projWRushingYards: { type: GraphQLFloat },
+    projWRushingTouchdowns: { type: GraphQLFloat },
+    projWFumblesLost: { type: GraphQLFloat },
+    projWReceivingTargets: { type: GraphQLFloat },
+    projWReceptions: { type: GraphQLFloat },
+    projWReceivingYards: { type: GraphQLFloat },
+    projWReceivingTouchdowns: { type: GraphQLFloat },
+    projWTwoPointConversionPasses: { type: GraphQLFloat },
+    projWTwoPointConversionRuns: { type: GraphQLFloat },
+    projWTwoPointConversionReceptions: { type: GraphQLFloat },
+    projWFantasyPoints: { type: GraphQLFloat },
+    projWFantasyPointsPPR: { type: GraphQLFloat },
+    projWActive: { type: GraphQLBoolean },
+
+    weeklyPassingAttempts: { type: GraphQLFloat },
+    weeklyPassingCompletions: { type: GraphQLFloat },
+    weeklyPassingYards: { type: GraphQLFloat },
+    weeklyPassingTouchdowns: { type: GraphQLFloat },
+    weeklyPassingInterceptions: { type: GraphQLFloat },
+    weeklypRushingAttempts: { type: GraphQLFloat },
+    weeklyRushingYards: { type: GraphQLFloat },
+    weeklyRushingTouchdowns: { type: GraphQLFloat },
+    weeklyFumblesLost: { type: GraphQLFloat },
+    weeklyReceivingTargets: { type: GraphQLFloat },
+    weeklyReceptions: { type: GraphQLFloat },
+    weeklyReceivingYards: { type: GraphQLFloat },
+    weeklyReceivingTouchdowns: { type: GraphQLFloat },
+    weeklyTwoPointConversionPasses: { type: GraphQLFloat },
+    weeklyTwoPointConversionRuns: { type: GraphQLFloat },
+    weeklyTwoPointConversionReceptions: { type: GraphQLFloat },
+    weeklyFantasyPoints: { type: GraphQLFloat },
+    weeklyFantasyPointsPPR: { type: GraphQLFloat },
+    weeklyActive: { type: GraphQLBoolean },
+
+    seasonPassingAttempts: { type: GraphQLFloat },
+    seasonPassingCompletions: { type: GraphQLFloat },
+    seasonPassingYards: { type: GraphQLFloat },
+    seasonPassingTouchdowns: { type: GraphQLFloat },
+    seasonPassingInterceptions: { type: GraphQLFloat },
+    seasonpRushingAttempts: { type: GraphQLFloat },
+    seasonRushingYards: { type: GraphQLFloat },
+    seasonRushingTouchdowns: { type: GraphQLFloat },
+    seasonFumblesLost: { type: GraphQLFloat },
+    seasonReceivingTargets: { type: GraphQLFloat },
+    seasonReceptions: { type: GraphQLFloat },
+    seasonReceivingYards: { type: GraphQLFloat },
+    seasonReceivingTouchdowns: { type: GraphQLFloat },
+    seasonTwoPointConversionPasses: { type: GraphQLFloat },
+    seasonTwoPointConversionRuns: { type: GraphQLFloat },
+    seasonTwoPointConversionReceptions: { type: GraphQLFloat },
+    seasonFantasyPoints: { type: GraphQLFloat },
+    seasonFantasyPointsPPR: { type: GraphQLFloat },
+
+    projSPassingAttempts: { type: GraphQLFloat },
+    projSPassingCompletions: { type: GraphQLFloat },
+    projSPassingYards: { type: GraphQLFloat },
+    projSPassingTouchdowns: { type: GraphQLFloat },
+    projSPassingInterceptions: { type: GraphQLFloat },
+    projSpRushingAttempts: { type: GraphQLFloat },
+    projSRushingYards: { type: GraphQLFloat },
+    projSRushingTouchdowns: { type: GraphQLFloat },
+    projSFumblesLost: { type: GraphQLFloat },
+    projSReceivingTargets: { type: GraphQLFloat },
+    projSReceptions: { type: GraphQLFloat },
+    projSReceivingYards: { type: GraphQLFloat },
+    projSReceivingTouchdowns: { type: GraphQLFloat },
+    projSTwoPointConversionPasses: { type: GraphQLFloat },
+    projSTwoPointConversionRuns: { type: GraphQLFloat },
+    projSTwoPointConversionReceptions: { type: GraphQLFloat },
+    projSFantasyPoints: { type: GraphQLFloat },
+    projSFantasyPointsPPR: { type: GraphQLFloat },
+//this user relation will be changed abit I think
+    user: {
+      type: UserType,
+      resolve(parentValue) {
+        return User.findById(parentValue.user).then((user) => user);
+      }
+    },
+  }),
+});
+
+module.exports = PlayerType;
