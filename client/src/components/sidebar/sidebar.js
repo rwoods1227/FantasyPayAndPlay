1b4f591dca6e9ea10bd65d9492d8c5cfd7a71cba
zach <zachcwilliams@icloud.com>
2020-01-13 14:59:46 -0800

    fix sidebar order

--- client/src/components/sidebar/sidebar.js
+++ client/src/components/sidebar/sidebar.js
@@ -21,22 +21,9 @@ const SideBar = ({ changeMain }) => {
       <ul>
         <li>
           <div
-            className={`sidebar-item${sidebar.selected === "league" ? " selected" : ""}`}
-            onClick={() => {
-              setSelected(sidebar => ({...sidebar, selected: "league"}));
-              changeMain(LeagueSplash, { changeMain });
-            }}
-          >
-            <div className="sidebar-item-icon league"></div>
-            <div className="sidebar-item-content">
-              <h3>League</h3>
-              <span>Create or join a league</span>
-            </div>
-          </div>
-        </li>
-        <li>
-          <div 
-            className={`sidebar-item${sidebar.selected === "bets" ? " selected" : ""}`}
+            className={`sidebar-item${
+              sidebar.selected === "bets" ? " selected" : ""
+            }`}
             onClick={() => {
               setSelected(sidebar => ({ ...sidebar, selected: "bets" }));
               changeMain(BetsIndex, { changeMain });
@@ -52,7 +39,10 @@ const SideBar = ({ changeMain }) => {
         <li>
           <Link to={`/credits`}>
             <div className="sidebar-item credits-sidebar-option">
-              <img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" className="sidebar-item-icon bets sidebar-credits-icon"></img>
+              <img
+                src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
+                className="sidebar-item-icon bets sidebar-credits-icon"
+              ></img>
               <div className="sidebar-item-content">
                 <h3>Credits</h3>
                 <span>Check out our Github accounts</span>
@@ -60,39 +50,67 @@ const SideBar = ({ changeMain }) => {
             </div>
           </Link>
         </li>
+        <li>
+          <div
+            className={`sidebar-item${
+              sidebar.selected === "league" ? " selected" : ""
+            }`}
+            onClick={() => {
+              setSelected(sidebar => ({ ...sidebar, selected: "league" }));
+              changeMain(LeagueSplash, { changeMain });
+            }}
+          >
+            <div className="sidebar-item-icon league"></div>
+            <div className="sidebar-item-content">
+              <h3>League</h3>
+              <span>Create or join a league</span>
+            </div>
+          </div>
+        </li>
       </ul>
       <div className="league-index">
         <h2>Leagues</h2>
         <div className="league-list-container">
-          <Query 
+          <Query
             query={FETCH_USER_LEAGUES}
-            variables={{ _id: localStorage.getItem("currentUserId")}}
+            variables={{ _id: localStorage.getItem("currentUserId") }}
             pollInterval={1000}
-          >{({ loading, error, data }) => {
-            if (loading) return <p>Loading...</p>;
-            if (error) return <p>Error</p>;
+          >
+            {({ loading, error, data }) => {
+              if (loading) return <p>Loading...</p>;
+              if (error) return <p>Error</p>;
 
-            return (
-              <div className="league-list">
-                <ul>
-                  {data.user.leagues.map(league => (
-                    <li key={league._id}>
-                      <div className={`sidebar-item${sidebar.selected === league._id ? " selected" : ""}`}
-                        onClick={() => {
-                          setSelected(sidebar => ({ ...sidebar, selected: league._id }));
-                          changeMain(LeagueShow, { changeMain, _id: league._id });
-                        }}>
-                        <div className="sidebar-item-content">
-                          <h3>{league.name}</h3>
-                          <span>{league.description}</span>
+              return (
+                <div className="league-list">
+                  <ul>
+                    {data.user.leagues.map(league => (
+                      <li key={league._id}>
+                        <div
+                          className={`sidebar-item${
+                            sidebar.selected === league._id ? " selected" : ""
+                          }`}
+                          onClick={() => {
+                            setSelected(sidebar => ({
+                              ...sidebar,
+                              selected: league._id
+                            }));
+                            changeMain(LeagueShow, {
+                              changeMain,
+                              _id: league._id
+                            });
+                          }}
+                        >
+                          <div className="sidebar-item-content">
+                            <h3>{league.name}</h3>
+                            <span>{league.description}</span>
+                          </div>
                         </div>
-                      </div>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            )
-          }}
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              );
+            }}
           </Query>
         </div>
       </div>
