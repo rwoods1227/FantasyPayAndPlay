958a0a19be290436d7c8ea5cd3cd6afaddb15877
Ryan Woods <ryanwoods14@gmail.com>
2020-02-03 19:21:59 -0500

    added alerts and conditions to league creation

--- client/src/components/league/CreateLeagueForm.js
+++ client/src/components/league/CreateLeagueForm.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { Formik, Form, Field } from 'formik';
 import { Mutation } from "react-apollo";
+import { useAlert } from 'react-alert'
 
 import Mutations from "../../graphql/mutations";
 const { NEW_LEAGUE } = Mutations;
 
 const CreateLeagueForm = () => {
+  const alert = useAlert();
   return (
     <Mutation mutation={NEW_LEAGUE}>
+      
       {(newLeague, data) => (
         <Formik
           initialValues={{
@@ -16,8 +19,17 @@ const CreateLeagueForm = () => {
             comissioner: localStorage.getItem("currentUserId")
           }}
           onSubmit={values => {
-            newLeague({ variables: values })
-              .then();
+            {
+              console.log(values)
+              if (values.name.length > 2 && values.description.length > 2) {
+                newLeague({ variables: values
+                }).then(() => {
+                  alert.show("League Created Successfully");
+                })
+              } else {
+                alert.show("Error! Name or Description Too Short")
+              }
+            }
           }}
         >
           <Form className="league-create-form">
